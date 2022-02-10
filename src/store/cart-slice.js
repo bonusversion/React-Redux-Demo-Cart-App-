006c3cb303dd7d277cc3ed49765a4ff109daac38
mybonus <506832550@qq.com>
2022-02-10 18:23:12 +0800

    redux fundamental

---
+++ src/store/cart-slice.js
@@ -0,0 +1,41 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => newItem.id === item.id);
+      state.totalQuantity++;
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += existingItem.price;
+      }
+    },
+    removeItemFromCart(state, action) {
+      state.totalQuantity--;
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter(item.id !== id);
+      } else {
+        existingItem.quantity--;
+      }
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
---
+++ src/store/ui-slice.js
@@ -0,0 +1,32 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const uiSlice = createSlice({
+//   name: "ui",
+//   initialState: {
+//     cartIsVisible: false,
+//   },
+//   reducers: {
+//     toggle(state) {
+//       state.cartIsVisible = !state.cartIsVisible;
+//     },
+//   },
+// });
+
+// export const uiActions = uiSlice.actions;
+// export default uiSlice;
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: { cartIsVisible: false },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
+    },
+  },
+});
+
+export const uiActions = uiSlice.actions;
+
+export default uiSlice;
